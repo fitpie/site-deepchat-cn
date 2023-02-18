2a2881e7b846ad931fb3f92b6a74c1563a429c13
fitpie <yf@fitpie.com>
2023-02-18 21:34:50 +0800

    Update locales

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,13 @@
 export default {
   title: ' ',
-  description: 'Powerful AI Chatbot app',
+  description: '智能聊天机器人',
   head: [
-    [
-      'link',
-      { rel: 'icon', type: 'image/x-icon',  href: '/docs/public/favicon.ico' }
-    ]
+    ['link', { rel: 'icon', type: 'image/x-icon',  href: '/docs/public/favicon.ico' }]
   ],
+  locales: {
+      root: { label: '简体中文' },
+      zh: { label: 'English', link: 'https://deepchat.app' }
+  },
   themeConfig: {
   	logo: '/logo.png',
     footer: {
